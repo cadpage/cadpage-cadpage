d8004ca13261362a80272b4e74ec448b96dca073
Ken Corbin <ken@cadpage.org>
2016-11-08 16:22:55 -0800

    Fixed parsign problem with Fayette County, PA

--- src/main/java/net/anei/cadpage/vendors/Active911Vendor.java
+++ src/main/java/net/anei/cadpage/vendors/Active911Vendor.java
@@ -1172,6 +1172,7 @@ class Active911Vendor extends Vendor {
       "US/PA/DelcoTextRelay",             "PADelawareCountyB",
       "US/PA/Ecm",                        "PAAlleghenyCountyB",
       "US/PA/ElkCounty",                  "PAElkCounty",
+      "US/PA/FayetteCounty",              "PAFayetteCounty",
       "US/PA/FDCMS",                      "PAChesterCountyF,PAChesterCountyO",
       "US/PA/Firepaging",                 "PAChesterCountyD4",
       "US/PA/FlourtownFire",              "PAMontgomeryCountyD",
