dce13ec032c9c80fda92e29ca78af4d9cfed13f3
Ken Corbin <ken@cadpage.org>
2016-12-29 10:46:45 -0800

    Fixed parsing problem witih Caldwell County, NC

--- src/main/java/net/anei/cadpage/vendors/Active911Vendor.java
+++ src/main/java/net/anei/cadpage/vendors/Active911Vendor.java
@@ -854,6 +854,7 @@ class Active911Vendor extends Vendor {
       "US/NC/BrunswickCounty",            "NCBrunswickCounty",
       "US/NC/BuncombeCounty",             "NCBuncombeCounty",
       "US/NC/CabarrusCounty",             "NCCabarrusCounty",
+      "US/NC/CaldwellCounty",             "NCCaldwellCounty",
       "US/NC/CarteretCounty",             "NCCarteretCounty",
       "US/NC/CaswellCounty",              "NCCaswellCounty",
       "US/NC/CatawbaCounty",              "NCCatawbaCounty",
