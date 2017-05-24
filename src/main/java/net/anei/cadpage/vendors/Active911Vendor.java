f5d6b4413e8e138e113b32366609efbfbb08ee44
Ken Corbin <ken@cadpage.org>
2017-05-23 18:29:23 -0700

    Fixed parsing problem with Baldwin County, AL

--- src/main/java/net/anei/cadpage/vendors/Active911Vendor.java
+++ src/main/java/net/anei/cadpage/vendors/Active911Vendor.java
@@ -218,6 +218,7 @@ class Active911Vendor extends Vendor {
       "US/AK/MatanuskaSusitna",           "AKMatanuskaSusitnaBorough",
       "US/AL/AlexanderCity",              "ALTallapoosaCounty",
       "US/AL/AthensLimestoneCounty",      "ALLimestoneCountyA",
+      "US/AL/BaldwinCounty",              "ALBaldwinCounty",
       "US/AL/BibbCounty",                 "ALBibbCounty",
       "US/AL/BlountCounty",               "ALBlountCounty",
       "US/AL/CalhounCounty",              "ALCalhounCounty",
