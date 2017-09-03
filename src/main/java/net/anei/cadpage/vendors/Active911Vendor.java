b72492d39e8035a5710bfd34c3aa6b99ba0e20e1
Ken Corbin <ken@cadpage.org>
2017-09-03 16:53:52 -0700

    Fixed parsing probelm with Kane County, IL (C)

--- src/main/java/net/anei/cadpage/vendors/Active911Vendor.java
+++ src/main/java/net/anei/cadpage/vendors/Active911Vendor.java
@@ -527,7 +527,7 @@ class Active911Vendor extends Vendor {
       "US/IL/GurneeFire",                 "ILLakeCountyB",
       "US/IL/HancockCounty",              "ILHancockCounty",
       "US/IL/JacksonCounty",              "ILJacksonCounty",
-      "US/IL/KaneCounty",                 "ILKaneCounty",
+      "US/IL/KaneCounty",                 "ILKaneCountyC",
       "US/IL/KankakeeCounty",             "ILKankakeeCounty",
       "US/IL/LakeCounty",                 "ILLakeCounty",
       "US/IL/MadisonCounty",              "ILMadisonCounty",
