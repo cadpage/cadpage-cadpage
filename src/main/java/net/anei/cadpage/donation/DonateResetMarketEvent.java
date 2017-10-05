9dba6d62ae0795a32ce21ae6201facbfcef41eda
Ken Corbin <ken@cadpage.org>
2017-10-04 20:35:14 -0700

    Fixed problem with recalcualte payment status not turning on account access

--- src/main/java/net/anei/cadpage/donation/DonateResetMarketEvent.java
+++ src/main/java/net/anei/cadpage/donation/DonateResetMarketEvent.java
@@ -7,25 +7,20 @@ import net.anei.cadpage.SmsPopupUtils;
 /**
 Recalculate Purchase Status
  */
-public class DonateResetMarketEvent extends DonateEvent {
+public class DonateResetMarketEvent extends AccountScreenEvent {
   
   public DonateResetMarketEvent() {
-    super(AlertStatus.YELLOW, R.string.donate_reset_market_title);
+    super(AlertStatus.YELLOW, R.string.donate_reset_market_title,
+        new AllowAcctPermissionAction() {
+          @Override
+          public void doEvent(Activity activity) {
+
+            // Request complete status reload
+            DonationManager.instance().refreshStatus(activity);
+          }
+        });
   }
 
-  @Override
-  protected void doEvent(Activity activity) {
-    
-    // Don't do anything if we aren't hooked to network
-    if (!SmsPopupUtils.haveNet(activity)) return;
-    
-    // Request complete status reload
-    DonationManager.instance().refreshStatus(activity);
-    
-    // Close donation screens
-    closeEvents(activity);
-  }
-  
   private static final DonateResetMarketEvent instance = new DonateResetMarketEvent();
   
   public static DonateResetMarketEvent instance() {
