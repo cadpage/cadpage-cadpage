f571bf5c1a5713bab0d3a154d39d718e5101b8ab
Ken Corbin <ken@cadpage.org>
2017-10-11 09:54:55 -0700

    Fixed parsing probelm with King County, WA (A)

--- src/main/java/net/anei/cadpage/vendors/Active911Vendor.java
+++ src/main/java/net/anei/cadpage/vendors/Active911Vendor.java
@@ -1608,7 +1608,7 @@ class Active911Vendor extends Vendor {
       "US/WA/IslandCounty",               "WAIslandCounty",
       "US/WA/JeffersonCounty",            "WAJeffersonCounty",
       "US/WA/KentRegional",               "WAKingCounty",
-      "US/WA/KingCounty",                 "WAKingCountyA",
+      "US/WA/KingCounty",                 "WAKingCountyA,WAKingCountyD,WASnohomishCountyE",
       "US/WA/KitsapCounty",               "WAKitsapCounty",
       "US/WA/KlickitatCounty",            "WAKlickitatCounty",
       "US/WA/LewisCounty",                "WALewisCounty",
