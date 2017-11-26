9bc1b152308c49d876d5b0f552a35d1b0a9d5cd5
Ken Corbin <ken@cadpage.org>
2017-11-26 12:57:37 -0800

    Release 1.9.15-01

--- src/main/java/net/anei/cadpage/SmsPopupConfigActivity.java
+++ src/main/java/net/anei/cadpage/SmsPopupConfigActivity.java
@@ -20,7 +20,6 @@ import net.anei.cadpage.preferences.LocationListPreference;
 import net.anei.cadpage.preferences.LocationManager;
 import net.anei.cadpage.preferences.OnDataChangeListener;
 import net.anei.cadpage.preferences.OnDialogClosedListener;
-import net.anei.cadpage.preferences.ValueTranslator;
 import net.anei.cadpage.vendors.VendorManager;
 import android.app.Activity;
 import android.app.AlertDialog;
