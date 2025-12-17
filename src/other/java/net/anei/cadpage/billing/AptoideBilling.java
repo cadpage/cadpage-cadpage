9b57ed5cc93a81c7db73cb766b50d9cbc6a50c67
Ken Corbin <ken@cadpage.org>
2025-12-17 11:46:03 -0800

    Created new variant for Apkpure (and any others)

---
+++ src/other/java/net/anei/cadpage/billing/AptoideBilling.java
@@ -0,0 +1,37 @@
+package net.anei.cadpage.billing;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
+@SuppressWarnings({"SpellCheckingInspection"})
+class AptoideBilling extends Billing {
+
+  @Override
+  public void initialize(Context context) {}
+
+  @Override
+  public void initialize(Activity activity) {}
+
+  @Override
+  public void destroy() {}
+
+  @Override
+  public boolean isReady() {
+    return false;
+  }
+
+  @Override
+  void doConnect() {}
+
+  @Override
+  void doRestoreTransactions(Context context) {}
+
+  @Override
+  void doStartPurchase(BillingActivity activity) {
+    throw new RuntimeException("Aptoide billing is not currently implemented");
+  }
+
+  @Override
+  void onActivityResult(int requestCode, int resultCode, Intent data) {}
+}
